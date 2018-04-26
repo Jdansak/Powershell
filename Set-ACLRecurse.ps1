9a013bd0c493cd2f9910294b22bbe032cdfc2af6
John Dansak <john.dansak@outlook.com>
2018-04-25 22:52:04 -0400

    first commit

---
+++ Set-ACLRecurse.ps1
@@ -0,0 +1,10 @@
+$right = "ReadandExecute"
+$principal = "IIS-IUSRS"
+$startdir = "D:\sites\"
+
+foreach($file in $(Get-ChildItem $startdir -Recurse)){
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule($principal,$right,"Allow")
+    $acl = get-acl $file.fullname
+    $acl.SetAccessRule($rule)
+    Set-Acl $file.fullname $acl
+    }
